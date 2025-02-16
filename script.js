15de9529836b20677faa982db5afb05999a70174
lonussss <lj2369@nyu.edu>
2025-02-16 17:37:55 +0400

    bug fixes for photo gallery

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ function closeModal() {
     document.getElementById("photoModal").style.display = "none";
 }
 
+// Prevent modal from closing when clicking the image
+document.getElementById("modalImage").addEventListener("click", function(event) {
+    event.stopPropagation();
+});
+
 // Event listener to close modal when pressing the Escape key
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
